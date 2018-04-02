e5e01211ea65d9cdfabfc065f6473d94dde17f24
itingkong <luoqi20131001@163.com>
2018-04-02 13:57:23 +0800

    头部公共组件

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
 import './style.less'
-
-class HomeHeader extends React.Component {
-    constructor(props, context) {
+class HomeHeader extends React.Component{
+    constructor(props, context){
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-    }
-    render() {
-        return (
+    };
+
+    render(){
+        return(
+
             <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
                     <span>{this.props.cityName}</span>
-                    &nbsp;
                     <i className="icon-angle-down"></i>
                 </div>
                 <div className="home-header-right float-right">
                     <i className="icon-user"></i>
                 </div>
                 <div className="home-header-middle">
-                    <div className="search-container">
-                        <i className="icon-search"></i>
-                        <input type="text" placeholder="请输入关键字"/>
-                    </div>
+                    <i className="icon-search"></i>
+                    <input placeholder="请输入商品名称" />
                 </div>
             </div>
         )
